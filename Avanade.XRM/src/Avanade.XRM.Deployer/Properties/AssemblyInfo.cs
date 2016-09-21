90e5e50269a27eaa3cbdd33029a40366bcb4e16f
Giancarlo Lelli <gcarlo.lelli@live.com>
2016-09-21 22:24:23 +0200

    Added automatic version bump on build

--- Avanade.XRM/src/Avanade.XRM.Deployer/Properties/AssemblyInfo.cs
+++ Avanade.XRM/src/Avanade.XRM.Deployer/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Avanade CI/CD for Dynamics CRM")]
 [assembly: AssemblyDescription("CI/CD of WebResources for Dynamics CRM")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Avanade Italy srl")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyProduct("CRM Deployer")]
 [assembly: AssemblyCopyright("Copyright Giancarlo Lelli ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("71936764-b366-4331-a310-bddd12685e71")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
