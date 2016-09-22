7e366c029d038b2f8805cc3346312139b80717c5
Giancarlo Lelli <giancarlo.lelli@avanade.com>
2016-09-22 10:29:01 +0200

    Initial commit

---
+++ Avanade.XRM/src/Avanade.XRM.Deployer/Common/ConfigurationProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+
+namespace Avanade.XRM.Deployer.Common
+{
+	public class ConfigurationProvider
+	{
+		public static string GetConnectionString(string key)
+		{
+			string value = string.Empty;
+
+			try
+			{
+				if (ConfigurationManager.ConnectionStrings[key] != null)
+				{
+					value = ConfigurationManager.ConnectionStrings[key].ConnectionString;
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+
+			return value;
+		}
+
+		public static string GetAppSettingsValue(string key)
+		{
+			string value = string.Empty;
+
+			try
+			{
+				if (ConfigurationManager.AppSettings[key] != null)
+				{
+					value = ConfigurationManager.AppSettings[key];
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+
+			return value;
+		}
+	}
+}
