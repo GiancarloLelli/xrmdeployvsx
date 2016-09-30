b71b161f00538e0c00c9013255b2835cbaf49b49
Giancarlo Lelli <gcarlo.lelli@live.com>
2016-09-30 17:21:32 +0200

    relocate some business logic

--- Avanade.XRM/src/Avanade.XRM.Deployer/CRM/WebResourceContainer.cs
+++ Avanade.XRM/src/Avanade.XRM.Deployer/CRM/WebResourceContainer.cs
@@ -1,5 +1,8 @@
-﻿using Avanade.XRM.Deployer.Model;
+﻿using Avanade.XRM.Deployer.Extensions;
+using Avanade.XRM.Deployer.Model;
+using Avanade.XRM.Deployer.Service;
 using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.Xrm.Sdk;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,6 +13,7 @@ namespace Avanade.XRM.Deployer.CRM
 	public class WebResourceContainer
 	{
 		readonly PendingChange[] m_changes;
+		readonly XrmWrapper m_context;
 
 		public int TotalChanges { get { return m_changes.Count(); } }
 		public int AddedItems { get { return m_changes.Count(p => p.ChangeTypeName.ToLower().Equals("add")); } }
@@ -18,9 +22,10 @@ namespace Avanade.XRM.Deployer.CRM
 
 		public List<CrmWebResource> WebResources { get; private set; }
 
-		public WebResourceContainer(PendingChange[] changes, string prefix)
+		public WebResourceContainer(PendingChange[] changes, string prefix, XrmWrapper wrapper)
 		{
 			m_changes = changes;
+			m_context = wrapper;
 			WebResources = GetWebResourceFromChanges(prefix);
 		}
 
@@ -49,6 +54,38 @@ namespace Avanade.XRM.Deployer.CRM
 			}
 
 			return resources;
+		}
+
+		public void EnsureContinue(string solution)
+		{
+			if (AddedItems > 0)
+			{
+				var solutionData = m_context.GetSolutionByName(solution);
+				if (solutionData.Id == Guid.Empty)
+				{
+					throw new Exception($"No solution found in CRM with unique name: '{solution}'");
+				}
+			};
+		}
+
+		public List<OrganizationRequest> BuildRequestList(string solution)
+		{
+			List<OrganizationRequest> requests = new List<OrganizationRequest>();
+
+			foreach (var resource in WebResources)
+			{
+				if (string.IsNullOrEmpty(resource.FullName)) continue;
+				var factory = m_context.RequestFactory(resource.ChangeType, resource.FullName, resource.ToEntity(), solution);
+
+				OrganizationRequest request = factory.Item1;
+				OrganizationRequest publishRequest = factory.Item2;
+
+				request.SetIfNotNull("SolutionUniqueName", solution);
+				requests.AddIfNotNull(request);
+				requests.AddIfNotNull(publishRequest);
+			}
+
+			return requests;
 		}
 	}
 }
--- Avanade.XRM/src/Avanade.XRM.Deployer/Program.cs
+++ Avanade.XRM/src/Avanade.XRM.Deployer/Program.cs
@@ -1,14 +1,9 @@
 ﻿using Avanade.XRM.Deployer.Auth;
 using Avanade.XRM.Deployer.Common;
 using Avanade.XRM.Deployer.CRM;
-using Avanade.XRM.Deployer.Extensions;
 using Avanade.XRM.Deployer.Service;
-using Microsoft.TeamFoundation.Client;
-using Microsoft.TeamFoundation.VersionControl.Client;
-using Microsoft.Xrm.Sdk;
+using Avanade.XRM.Deployer.TFS;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Avanade.XRM.Deployer
 {
@@ -29,66 +24,21 @@ namespace Avanade.XRM.Deployer
 				var context = new XrmWrapper(bootstrap.CRMKey);
 				var userName = !string.IsNullOrEmpty(bootstrap.User) ? bootstrap.User : Environment.UserName;
 				var workspaceName = !string.IsNullOrEmpty(bootstrap.Workspace) ? bootstrap.Workspace : Environment.MachineName;
+				var sourceControl = new SourceControlManager(workspaceName, userName);
+				var sourceControlResult = sourceControl.InitializeWorkspace(bootstrap.TFS, AuthHelper.GetCredentials(bootstrap));
 
-				using (TfsTeamProjectCollection collection = new TfsTeamProjectCollection(bootstrap.TFS, AuthHelper.GetCredentials(bootstrap)))
-				{
-					Console.WriteLine($"[LOG] => Enumerazione cambiamenti sul Workspace.");
-					VersionControlServer versionControl = collection.GetService(typeof(VersionControlServer)) as VersionControlServer;
-					Workspace workspace = versionControl.GetWorkspace(workspaceName, userName);
+				// Must resolve conflicts
+				if (!sourceControlResult.Continue) return;
 
-					PendingChange[] changes = workspace.GetPendingChanges();
-					container = new WebResourceContainer(changes, bootstrap.Prefix);
-					Console.WriteLine($"[LOG] => Trovate {container.TotalChanges} Check-Out items.");
-
-					var folderFilters = changes.Select(c => c.LocalOrServerFolder).Distinct().ToArray();
-					if (folderFilters.Length > 0)
-					{
-						var conflicts = workspace.QueryConflicts(folderFilters, true);
-						if (conflicts.Length > 0)
-						{
-							Console.WriteLine($"[LOG] => Rilevati {conflicts.Length} conflitti. Aggiorna il workspace prima di continuare");
-							return;
-						}
-					}
-
-					if (changes.Length > 0)
-					{
-						workspace.CheckIn(changes, $"CI Checkin by {Environment.UserName}");
-						Console.WriteLine($"[LOG] => Check-In effettuato on behalf of {Environment.UserName}.");
-					}
-				}
-
+				container = new WebResourceContainer(sourceControlResult.Changes, bootstrap.Prefix, context);
 				Console.WriteLine($"[LOG] => Trovate {container.WebResources.Count} Web Resource.");
 				Console.WriteLine($"[STS] => Add: {container.AddedItems} - Edit: {container.EditedItems} - Delete: {container.DeletedItems}");
 				Console.WriteLine($"[LOG] => '{bootstrap.Prefix}' utilizzato come root.");
 				Console.WriteLine($"[LOG] => Fetch soluzione '{bootstrap.Solution}' da CRM.");
+				container.EnsureContinue(bootstrap.Solution);
 
-				if (container.AddedItems > 0)
-				{
-					var solution = context.GetSolutionByName(bootstrap.Solution);
-					if (solution.Id == Guid.Empty)
-					{
-						throw new Exception($"No solution found in CRM with unique name: '{bootstrap.Solution}'");
-					}
-				};
-
-				Console.WriteLine($"[LOG] => Generazione pool OrganizationRequest.");
-				List<OrganizationRequest> requests = new List<OrganizationRequest>();
-				foreach (var resource in container.WebResources)
-				{
-					if (string.IsNullOrEmpty(resource.FullName)) continue;
-					var factory = context.RequestFactory(resource.ChangeType, resource.FullName, resource.ToEntity(), bootstrap.Solution);
-
-					OrganizationRequest request = factory.Item1;
-					OrganizationRequest publishRequest = factory.Item2;
-
-					request.SetIfNotNull("SolutionUniqueName", bootstrap.Solution);
-					requests.AddIfNotNull(request);
-					requests.AddIfNotNull(publishRequest);
-				}
-
-				Console.WriteLine($"[LOG] => Scrittura su CRM.");
-				context.Flush(requests);
+				Console.WriteLine($"[LOG] => Generazione pool OrganizationRequest & scrittura su CRM.");
+				context.Flush(container.BuildRequestList(bootstrap.Solution));
 			}
 			catch (Exception exception)
 			{
